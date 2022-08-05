6a7e75d978081b38269c1243c337684c4dd189c3
worchard <34101144+worchard@users.noreply.github.com>
2022-08-05 17:27:54 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='pynem',
+    version='0.0.1',
+    description='Python implementation of Nested Effects Models',
+    long_description='PyNEM is a package for the learning and handling of Nested Effects Models',
+    author='William Orchard',
+    author_email='will.r.orchard@gmail.com',
+    packages=setuptools.find_packages(exclude=['tests']),
+    python_requires='>3.5.0',
+    zip_safe=False,
+    classifiers=[
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    install_requires=[
+        'numpy'
+    ]
+)
