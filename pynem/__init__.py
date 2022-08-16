ff32bae7985f8f38e55009dc7ab9f5f1845df741
William Orchard <orchar01@c02w31uvhv29.uantwerpen.be>
2022-08-16 10:42:43 +0200

    adding minimal docstring to package

--- pynem/__init__.py
+++ pynem/__init__.py
@@ -1,2 +1,8 @@
+"""
+PyNEM is the Python implementation of Nested Effects Models (NEMs). For the time being, it implements only the greedy weak order \
+algorithm for learning, scoring either using the marginal log-likelihood (mLL), or using an alternating optimisation scheme. \
+In time, I may also implement some of the major learning and scoring approaches from the original 'nem' package written in R.
+"""
+
 from .classes import *
 from .custom_types import *
--- pynem/classes/__init__.py
+++ pynem/classes/__init__.py
@@ -1,1 +1,1 @@
-from . import *
+from .signalgraph import *
