c0e9121c9cb7f0a04f93ecc31ecdc4f9d83d036b
worchard <will.r.orchard@gmail.com>
2022-10-06 12:19:41 +0100

    added unit tests for effectattachments

---
+++ tests/test_effectattachments.py
@@ -0,0 +1,41 @@
+from pynem import EffectAttachments
+import pytest
+import numpy as np
+
+def test_empty_effectattachments():
+    ea = EffectAttachments()
+    assert ea.data == {}
+    assert ea.signals == set()
+
+def test_signal_only_init():
+    ea = EffectAttachments(signals = {1,2,3})
+    assert ea.data == {}
+    assert ea.signals == {1,2,3}
+
+def test_fromkeys():
+    ea = EffectAttachments.fromkeys({1,2,3})
+    assert ea.data == {1: None, 2: None, 3: None}
+    assert ea.signals == {None}
+
+def test_fails_when_signal_not_hashable():
+    ea = EffectAttachments()
+    with pytest.raises(TypeError):
+        ea[1] = [3,4]
+
+def test_effectattachments_copy():
+    ea1 = EffectAttachments({1:2, 3:4, 5:6}, signals = {7,8,9})
+    ea2 = ea1.copy()
+    assert ea1 == ea2
+
+def test_rename_nodes():
+    ea1 = EffectAttachments({'E1':'S1', 'E2':'S2', 'E3':'S3'}, signals = {'S4'})
+    ea2 = ea1.rename_nodes(signal_map = {'S1': 'S1', 'S2':'S3', 'S3':'S4', 'S4':'S2'}, effect_map = {'E1': 'E1', 'E2': 'E3', 'E3':'E2'})
+    assert ea2.data == {'E1': 'S1', 'E3': 'S3', 'E2': 'S4'}
+    assert ea2.signals == {'S3', 'S2', 'S1', 'S4'}
+
+
+def test_to_adjacency():
+    ea = EffectAttachments({'E1':'S1', 'E2':'S2', 'E3':'S3'}, signals = {'S4','S5','S6'})
+    adj_test_mat = np.array([[0, 1], [0, 0], [0,0]])
+    adjacency_matrix, signal_list, effect_list = ea.to_adjacency(signal_list=['S1', 'S3', 'S4'], effect_list = ['E2', 'E1'])
+    assert np.all(adjacency_matrix == adj_test_mat)
