bd850a350e50b405dd3207b9fe7a69b9413bf9de
worchard <will.r.orchard@gmail.com>
2022-11-03 18:02:33 +0000

    Adding current properties, implementing repr and str

--- pynem/classes/nem.py
+++ pynem/classes/nem.py
@@ -1,4 +1,5 @@
 from typing import Hashable, Set, Union, Tuple, Any, Iterable, Dict, FrozenSet, List
+from os import linesep
 
 from pynem.utils import core_utils
 from pynem.custom_types import *
@@ -18,12 +19,13 @@ class NestedEffectsModel():
                 nem = None):
         self._controls = controls
         self._signalscolumn = signals_column
+        self._score = None
         if nem is not None:
             self._adata = nem._adata
             self._signals = nem._signals
             self._effects = nem._effects
-            self._signalgraph = nem._signalgraph
-            self._effectattachments = nem._effectattachments
+            self._signal_graph = nem._signal_graph
+            self._effect_attachments = nem._effect_attachments
         else:
             self._adata = adata
             self._signals = set(adata.obs[signals_column]).difference(controls)
@@ -35,27 +37,26 @@ class NestedEffectsModel():
                 self._effects.intersection_update(effects)
             
             if signal_graph:
-                self._signalgraph = signal_graph
+                self._signal_graph = signal_graph
                 self._signals = signal_graph.nodes
             else:
-                self._signalgraph = SignalGraph(nodes = self._signals)
+                self._signal_graph = SignalGraph(nodes = self._signals)
             if effect_attachments:
-                self._effectattachments = effect_attachments
+                self._effect_attachments = effect_attachments
                 self._effects = effect_attachments.effects()
             else:
-                self._effectattachments = EffectAttachments.fromeffects(self._effects, signals = self._signals)
+                self._effect_attachments = EffectAttachments.fromeffects(self._effects, signals = self._signals)
 
-            assert self._signalgraph.nodes == self._effectattachments.signals.difference({None}), \
+            assert self._signal_graph.nodes == self._effect_attachments.signals.difference({None}), \
                 "Nodes of the signal graph must match the signals of the effect attachments"
 
     def predict(self, return_array: bool = False) -> Union[dict, np.ndarray]:
         """
         Predict which effect reporters will be affcted by the perturbation of each signal, given a signal graph and effect attachments.
         """
-        
         pass
 
-    def score(self):
+    def score_model(self):
         raise NotImplementedError
 
     def copy(self):
@@ -69,3 +70,41 @@ class NestedEffectsModel():
 
     def transitive_reduction(self):
         raise NotImplementedError
+    
+    def __str__(self) -> str:
+        scr = self._score
+        dat = self._adata.__str__()
+        sg = self._signal_graph.__str__()
+        ea = self._effect_attachments.__str__()
+        out_string = "Nested Effects Model object" + linesep + f"Score: {scr}" + linesep + \
+        f"Data: {dat}" + linesep + f"Signal graph: {sg}" + linesep + f"Effect Attachments: {ea}"
+        return out_string
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    # === PROPERTIES
+
+    @property
+    def signals(self) -> Set[Node]:
+        return set(self._signals)
+    
+    @property
+    def effects(self) -> Set[Node]:
+        return set(self._effects)
+
+    @property
+    def signal_graph(self) -> SignalGraph:
+        return self._signal_graph.copy()
+    
+    @property
+    def effect_attachments(self) -> EffectAttachments:
+        return self._effect_attachments.copy()
+    
+    @property
+    def adata(self) -> ad.AnnData:
+        return self._adata.copy()
+
+    @property
+    def score(self) -> float:
+        return self._score
