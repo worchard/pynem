b8488173669480561dd07f677ba7a781ceca9fc4
worchard <will.r.orchard@gmail.com>
2023-01-19 18:56:18 +0000

    almost complete adata preprocessing function added

--- pynem/utils/core_utils.py
+++ pynem/utils/core_utils.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import numpy as np
+import anndata as ad
 from pynem.custom_types import *
 
 def defdict2dict(defdict, keys):
@@ -37,3 +38,22 @@ def get_unique_name(name, name_universe):
         name = f"{orig_name}_{i}"
         i += 1
   return name
+
+def preprocess_adata(adata: ad.AnnData, data_key = 'data', data_type_key = 'binary',
+                     names_array_key = 'names', groups: list = list(), genes: list = list()):
+    raise NotImplementedError
+    gene_names = list(adata.var.index)
+    if not np.all(genes in gene_names):
+        raise ValueError("Not all provided genes could be found in the data")
+    group_names = list(adata.uns[data_key][data_type_key].dtype.names)
+    if not np.all(groups in group_names):
+        raise ValueError("Not all provided groups could be found in the data")
+    names_array = adata.uns[data_key][names_array_key]
+    name_sort_array = np.argsort(np.array(names_array.tolist()).T)
+    data_array = np.array(adata.uns[data_key][data_type_key].tolist())
+    name_sorted_data_array = np.empty(data_array.shape)
+    for i in range(len(group_names)):
+        sort_idx = name_sort_array[i]
+        name_sorted_data_array[:,i] = data_array[:,i][sort_idx]
+    if genes:
+        pass
