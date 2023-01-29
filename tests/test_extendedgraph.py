f87c044c7eb07258d640d0745fcca31019c7671e
worchard <will.r.orchard@gmail.com>
2023-01-29 18:03:51 +0000

    more tests

--- tests/test_extendedgraph.py
+++ tests/test_extendedgraph.py
@@ -139,3 +139,60 @@ def test_children_of():
     eg._join_actions(0,1)
     assert {2, 3} == eg._children_of({0,1})
 
+def test_joined_to():
+    eg = ExtendedGraph(actions_amat = np.triu((1,1,1)))
+    for a in eg.actions_idx():
+        assert eg._joined_to(a) == a
+    eg._join_actions(0,1)
+    assert np.array_equal(np.array([0,1]), eg._joined_to(0))
+    assert np.array_equal(np.array([0,1]), eg._joined_to(1))
+
+def test_add_edge():
+    eg = ExtendedGraph(edges={(0,1), (0,2)})
+    assert {(0,0), (0,1), (1,1), (0,2), (2,2)} == set(eg.edges())
+    aamat = eg.add_edge(1,2, inplace=False)
+    eg.add_edge(1,2)
+    assert eg._actions_amat[1,2]
+    assert {(0,0), (0,1), (1,1), (1,2), (0,2), (2,2)} == set(eg.edges())
+    assert np.array_equal(aamat, eg._actions_amat)
+
+def test_add_edges_from():
+    edges = {(0,1), (1,2), (0,2)}
+    eg1 = ExtendedGraph(actions=[0,1,2])
+    aamat = eg1._add_edges_from(edges, inplace=False)
+    eg1._add_edges_from(edges)
+    eg2 = ExtendedGraph(edges = edges)
+    assert eg1 == eg2
+    assert np.array_equal(aamat, eg2._actions_amat)
+
+def test_remove_edge():
+    eg = ExtendedGraph(actions_amat=np.triu((1,1,1)))
+    assert eg._actions_amat[1,2]
+    assert {(0,0), (0,1), (1,1), (1,2), (0,2), (2,2)} == set(eg.edges())
+    aamat = eg.remove_edge(1,2, inplace=False)
+    eg.remove_edge(1,2)
+    assert not eg._actions_amat[1,2]
+    assert {(0,0), (0,1), (1,1), (0,2), (2,2)} == set(eg.edges())
+    assert np.array_equal(aamat, eg._actions_amat)
+
+def test_remove_edges_from():
+    eg1 = ExtendedGraph(actions_amat=np.triu((1,1,1)))
+    eg2 = ExtendedGraph(actions=[0,1,2], edges={(0,1)})
+    aamat = eg1.remove_edges_from({(1,2), (0,2)}, inplace=False)
+    eg1.remove_edges_from({(1,2), (0,2)})
+    assert eg1 == eg2
+    assert np.array_equal(aamat, eg2._actions_amat)
+
+def test_attach_effect():
+    eg = ExtendedGraph(attachments_amat=np.array([[1,1,0], [0,0,1]]))
+    aamat = eg.attach_effect(0, 2, inplace=False)
+    eg.attach_effect(0, 2)
+    assert np.array_equal(np.array([[1,1,1], [0,0,0]]), eg._attachments_amat)
+    assert np.array_equal(eg._attachments_amat, aamat)
+
+def test_attach_effects_from():
+    eg = ExtendedGraph(attachments_amat=np.array([[1,1,0], [0,0,1]]))
+    aamat = eg.attach_effects_from({(0,2), (1,0), (1,1)}, inplace=False)
+    eg.attach_effects_from({(0,2), (1,0), (1,1)})
+    assert np.array_equal(np.array([[0,0,1], [1,1,0]]), eg._attachments_amat)
+    assert np.array_equal(aamat, eg._attachments_amat)
